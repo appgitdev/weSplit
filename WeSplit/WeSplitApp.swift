7e87068a5a485586c09a30a8a20c21ed3249fcc3
ferdiangunawan <ferdian@majoo.id>
2022-09-10 02:46:03 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ferdian Gunawan on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
